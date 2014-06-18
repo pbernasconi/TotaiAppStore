fb82e9285639324abca9b24f1fdbd8609c67013b
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-06-18 17:03:12 +0200

    pub

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
-// Ionic Starter App
+angular.module('starter', [
+  'ionic',
+  'starter.controllers',
+  'starter.services'
+])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
-
-  .run(function ($ionicPlatform) {
+  .run(function ($rootScope, $ionicPlatform) {
     $ionicPlatform.ready(function () {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
       if (window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       }
       if (window.StatusBar) {
-        // org.apache.cordova.statusbar required
         StatusBar.styleDefault();
       }
     });
+
   })
+
 
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
+
+
+      .state('login', {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: 'LoginCtrl'
+      })
 
       .state('app', {
         url: "/app",
@@ -29,6 +33,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         templateUrl: "templates/menu.html",
         controller: 'AppCtrl'
       })
+
 
       .state('app.apps', {
         url: "/apps",
@@ -75,7 +80,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
           }
         }
       });
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/apps');
+
+    $urlRouterProvider.otherwise('login');
   });
 
--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,39 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope) {
-})
+  .controller('AppCtrl', function ($scope, $state) {
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
+    $scope.logOut = function () {
+      $state.go('login');
+    }
+  })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-})
+  .controller('LoginCtrl', function ($scope, $state) {
+
+    $scope.credentials = {
+      username: '',
+      password: ''
+    };
+
+    $scope.login = function(credentials) {
+      if (credentials.username == 'paolo' && credentials.password == 'pass') {
+        $state.go('app.apps');
+      }
+      else {
+        alert('Wrong username and password');
+      }
+    }
+  })
+
+  .controller('PlaylistsCtrl', function ($scope) {
+    $scope.playlists = [
+      { title: 'Reggae', id: 1 },
+      { title: 'Chill', id: 2 },
+      { title: 'Dubstep', id: 3 },
+      { title: 'Indie', id: 4 },
+      { title: 'Rap', id: 5 },
+      { title: 'Cowbell', id: 6 }
+    ];
+  })
+
+  .controller('PlaylistCtrl', function ($scope, $stateParams) {
+  });
